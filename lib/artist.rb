bd27197d89fed1e5c1c8582158bc545d92c9b37e
Min Kim <mk4555@nyu.edu>
2017-11-04 19:38:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,12 +8,12 @@ class Artist
 
   def add_song(song)
     @songs << song
-    @songs
+    song.name =
   end
 end
 
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
